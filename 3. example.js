4e4bbae5e0ed5ab205a027b2cd7e2a7079154b78
Jhanvi Ladiwala <105975195+JHANVILADIWALA@users.noreply.github.com>
2023-05-24 12:52:59 +0530

    Create 3. example.js

---
+++ 3. example.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <>
+   //these are not HTML
+  //these are JSX
+  <h1>Top Netflix Picks </h1> 
+  <p> Have a look at top ones: </p>
+  <ol>
+    <li>DARK</li>
+    <li>Vampire Diaries</li>
+    <li>Stranger Things</li>
+    <li>Money heist</li>
+  </ol>
+  </>
+,
+document.getElementById("root"));
