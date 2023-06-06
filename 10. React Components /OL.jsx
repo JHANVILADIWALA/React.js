846740dafd73ab78aee21f57ae7677c7ea1288ab
Jhanvi Ladiwala <jhanviladiwalapf@gmail.com>
2023-06-06 14:00:32 +0530

    Create OL.jsx

---
+++ 10. React Components /OL.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function OL() {
+    return (
+        <ol>
+            <li>first</li>
+            <li>second</li>
+            <li>third</li>
+            <li>fourth</li>
+        </ol>
+    )
+}
+export default OL;
