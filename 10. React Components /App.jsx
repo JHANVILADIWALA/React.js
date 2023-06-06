13eef6d93569267029e3185fc1afeb5a9a8ee447
Jhanvi Ladiwala <jhanviladiwalapf@gmail.com>
2023-06-06 13:58:57 +0530

    Create App.jsx

---
+++ 10. React Components /App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Heading from './Heading';
+import Para from './Para';
+import OL from './OL';
+
+function App() {
+    return (
+        <>
+            <Heading />
+            <Para />
+            <OL></OL>
+        </>
+    );
+}
+export default App;
