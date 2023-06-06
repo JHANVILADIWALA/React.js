87b14614eab9b20460a561b26c661619877155e6
Jhanvi Ladiwala <jhanviladiwalapf@gmail.com>
2023-06-06 13:58:30 +0530

    Create index.js

---
+++ 10. React Components /index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+ 
+//---------> KEEP 1st LETTER OF COMPONENT CAPITAL
+
+ReactDOM.render(
+<App />,document.getElementById('root')
+);
