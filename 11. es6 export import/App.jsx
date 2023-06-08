40f19d0290f3d5ba27629b2f11db49652fc2554d
Jhanvi Ladiwala <jhanviladiwalapf@gmail.com>
2023-06-08 16:37:47 +0530

    Create App.jsx

---
+++ 11. es6 export import/App.jsx
@@ -0,0 +1,11 @@
+const fruit='apple';
+const prog='react js';
+
+
+function color(){
+    let val="red";
+  return ( val );
+}
+
+export default fruit;  //only one default can be there
+export {prog,color};   //no parantheses
