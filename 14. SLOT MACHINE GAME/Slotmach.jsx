41a6d7a846e3cca4801337cf05da85c021471305
Jhanvi Ladiwala <jhanviladiwalapf@gmail.com>
2023-06-12 12:00:33 +0530

    Create Slotmach.jsx

---
+++ 14. SLOT MACHINE GAME/Slotmach.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const SlotM = (props) => {
+    let x = props.x;
+    let y = props.y;
+    let z = props.z;
+
+    if (x == y && y == z) {
+        return (
+            <>
+                <div className="slot_inner">
+                    <h1>{x}{y}{z}</h1>
+                    <h1>Matched !! </h1>
+                </div>
+            </>
+        )
+    }
+    else {
+        return (
+            <>
+                <div className="slot_inner">
+                    <h1>{x}{y}{z}</h1>
+                    <h1>Not Matched  !! </h1>
+                </div>
+            </>
+        )
+    }
+}
+
+export default SlotM;
